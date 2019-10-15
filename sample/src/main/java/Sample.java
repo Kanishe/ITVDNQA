840a758dd7882a81b8477e0250f8e8943684eace
Alexandr Kanishevskiy <a.kan4988@gmail.com>
2019-10-15 23:02:46 +0300

    добавил поиск по xpath
    добавил собственный поиск по xpath

--- sample/src/main/java/Sample.java
+++ sample/src/main/java/Sample.java
@@ -1,16 +1,21 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+
 
 public class Sample {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         // Указываем путь к драйверу. Это необходимо для всех драйверов кроме Firefox до 46 версии.
         System.setProperty("webdriver.chrome.driver","/Users/alexandrkanishevskii/IdeaProjects/ITVDNQA/sample/src/main/resources/chromedriver");
         WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(600, TimeUnit.SECONDS);
 
 
         // OnLoad событие
@@ -20,18 +25,23 @@ public class Sample {
         searchBox.sendKeys("pickles");
         searchBox.submit();
 
+        //
+        driver.get("https://www.google.ru/");
+        WebElement serchMiami = driver.findElement(By.xpath("//input[@name='q']"));
+        serchMiami.sendKeys("майами");
+        WebElement clickSearchMiami = driver.findElement(By.xpath("//input[@name='q']"));
+        clickSearchMiami.sendKeys(Keys.ENTER);
 
 
 
-
-        // Дополнительный способ перейти на страницу
-        // driver.get() == driver.navigate().to()
+//         Дополнительный способ перейти на страницу
+//         driver.get() == driver.navigate().to()
         driver.navigate().to("https://yandex.ru/");
         WebElement cambridgeSearchBox = driver.findElement(By.id("text"));
         cambridgeSearchBox.sendKeys("carrot");
         cambridgeSearchBox.submit();
-
-        // Основные функции навигации браузера
+//
+//         Основные функции навигации браузера
         driver.navigate().back();
         driver.navigate().refresh();
         driver.navigate().forward();
